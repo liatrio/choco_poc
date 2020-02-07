a9bf1efceeaa2c56554fe2a9ecc6835b82973320
Michael Richard <michaelr@liatrio.com>
2020-02-07 19:25:30 +0000

    Updating

--- example_pkgs/AssetExplorer/tools/chocolateyinstall.ps1
+++ example_pkgs/AssetExplorer/tools/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'ManageEngine_AssetExplorer_64bit.exe'
+$url           = "https://www.manageengine.com/products/asset-explorer/87652187/ManageEngine_AssetExplorer_64bit.exe"
 
 #Based on InstallShield
 $packageArgs = @{
@@ -10,7 +11,7 @@ $packageArgs = @{
   fileType      = 'exe'
   silentArgs    = "/s /v`"/qn`" /v`"REBOOT=ReallySuppress`" /sms"
   validExitCodes= @(0,1641,3010)
-  url           = "https://www.manageengine.com/products/asset-explorer/87652187/ManageEngine_AssetExplorer_64bit.exe"
+  url           = $url
   checksum      = '4DA09F4C5F3B8410BD4658C056A15F379C09BC0458304075A3E5745017D5046E'
   checksumType  = 'sha256'
   url64bit      = ""
@@ -19,6 +20,6 @@ $packageArgs = @{
   destination   = $toolsDir
   #installDir   = "" # passed when you want to override install directory - requires licensed editions 1.9.0+
 }
-
+Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath $fileLocation -Url $url
 Install-ChocolateyInstallPackage @packageArgs
 
